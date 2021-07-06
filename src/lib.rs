830fc1b8d5b638ff34b52ab98955d249f652eeee
Christopher Berner <christopherberner@gmail.com>
2021-07-06 14:53:32 -0700

    Implement basic binary-tree index

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod btree;
 mod db;
 mod error;
 #[cfg(feature = "python")]
--- src/storage.rs
+++ src/storage.rs
@@ -1,3 +1,4 @@
+use crate::btree::{lookup_in_raw, BtreeBuilder};
 use crate::Error;
 use memmap2::MmapMut;
 use std::cell::{Ref, RefCell};
@@ -5,8 +6,8 @@ use std::convert::TryInto;
 use std::ops::Deref;
 
 const MAGICNUMBER: [u8; 4] = [b'r', b'e', b'd', b'b'];
-const ENTRIES_OFFSET: usize = MAGICNUMBER.len();
-const DATA_OFFSET: usize = ENTRIES_OFFSET + 8;
+const DATA_LEN: usize = MAGICNUMBER.len();
+const DATA_OFFSET: usize = DATA_LEN + 8;
 
 pub(in crate) struct Storage {
     mmap: RefCell<MmapMut>,
@@ -24,7 +25,7 @@ impl Storage {
         if mmap[0..MAGICNUMBER.len()] == MAGICNUMBER {
             return Ok(());
         }
-        mmap[ENTRIES_OFFSET..(ENTRIES_OFFSET + 8)].copy_from_slice(&0u64.to_be_bytes());
+        mmap[DATA_LEN..(DATA_LEN + 8)].copy_from_slice(&0u64.to_be_bytes());
         mmap.flush()?;
         // Write the magic number only after the data structure is initialized and written to disk
         // to ensure that it's crash safe
@@ -36,28 +37,12 @@ impl Storage {
 
     pub(in crate) fn append(&self, key: &[u8], value: &[u8]) -> Result<(), Error> {
         let mut mmap = self.mmap.borrow_mut();
-        let mut entries = u64::from_be_bytes(
-            mmap[ENTRIES_OFFSET..(ENTRIES_OFFSET + 8)]
-                .try_into()
-                .unwrap(),
-        ) as usize;
+        let mut data_len =
+            u64::from_be_bytes(mmap[DATA_LEN..(DATA_LEN + 8)].try_into().unwrap()) as usize;
 
-        let mut index = DATA_OFFSET;
-        let mut entry = 0;
-        while entry < entries {
-            let key_len = u64::from_be_bytes(mmap[index..(index + 8)].try_into().unwrap()) as usize;
-            index += 8;
-            if key == &mmap[index..(index + key_len)] {
-                todo!("support overwriting existing keys");
-            }
-            index += key_len;
-            let value_len =
-                u64::from_be_bytes(mmap[index..(index + 8)].try_into().unwrap()) as usize;
-            index += 8 + value_len;
-            entry += 1;
-        }
+        let mut index = DATA_OFFSET + data_len;
 
-        // Does not exist, append the new key & value
+        // Append the new key & value
         mmap[index..(index + 8)].copy_from_slice(&(key.len() as u64).to_be_bytes());
         index += 8;
         mmap[index..(index + key.len())].copy_from_slice(key);
@@ -65,50 +50,60 @@ impl Storage {
         mmap[index..(index + 8)].copy_from_slice(&(value.len() as u64).to_be_bytes());
         index += 8;
         mmap[index..(index + value.len())].copy_from_slice(value);
+        index += value.len();
 
-        entries += 1;
-        mmap[ENTRIES_OFFSET..(ENTRIES_OFFSET + 8)].copy_from_slice(&entries.to_be_bytes());
+        data_len = index - DATA_OFFSET;
+
+        mmap[DATA_LEN..(DATA_LEN + 8)].copy_from_slice(&data_len.to_be_bytes());
         Ok(())
     }
 
     pub(in crate) fn fsync(&self) -> Result<(), Error> {
-        self.mmap.borrow().flush()?;
+        let mut builder = BtreeBuilder::new();
+        let mut mmap = self.mmap.borrow_mut();
+
+        let data_len =
+            u64::from_be_bytes(mmap[DATA_LEN..(DATA_LEN + 8)].try_into().unwrap()) as usize;
+
+        let mut index = DATA_OFFSET;
+        while index < (DATA_OFFSET + data_len) {
+            let key_len = u64::from_be_bytes(mmap[index..(index + 8)].try_into().unwrap()) as usize;
+            index += 8;
+            let key = &mmap[index..(index + key_len)];
+            index += key_len;
+            let value_len =
+                u64::from_be_bytes(mmap[index..(index + 8)].try_into().unwrap()) as usize;
+            index += 8;
+            let value = &mmap[index..(index + value_len)];
+            index += value_len;
+            builder.add(key, value);
+        }
+
+        let node = builder.build();
+        assert!(DATA_OFFSET + data_len + node.recursive_size() < mmap.len());
+        node.to_bytes(&mut mmap[(DATA_OFFSET + data_len)..], 0);
+
+        mmap.flush()?;
         Ok(())
     }
 
     pub(in crate) fn get(&self, key: &[u8]) -> Result<Option<AccessGuard>, Error> {
         let mmap = self.mmap.borrow();
 
-        let entries = u64::from_be_bytes(
-            mmap[ENTRIES_OFFSET..(ENTRIES_OFFSET + 8)]
-                .try_into()
-                .unwrap(),
-        ) as usize;
+        let data_len =
+            u64::from_be_bytes(mmap[DATA_LEN..(DATA_LEN + 8)].try_into().unwrap()) as usize;
 
-        let mut index = DATA_OFFSET;
-        let mut entry = 0;
-        while entry < entries {
-            let key_len = u64::from_be_bytes(mmap[index..(index + 8)].try_into().unwrap()) as usize;
-            index += 8;
-            let value_len = u64::from_be_bytes(
-                mmap[(index + key_len)..(index + key_len + 8)]
-                    .try_into()
-                    .unwrap(),
-            ) as usize;
-            if key == &mmap[index..(index + key_len)] {
-                index += key_len + 8;
-                let guard = AccessGuard {
-                    mmap_ref: mmap,
-                    offset: index,
-                    len: value_len,
-                };
-                return Ok(Some(guard));
-            }
-            index += key_len + 8 + value_len;
-            entry += 1;
+        let index = DATA_OFFSET + data_len;
+        if let Some((offset, len)) = lookup_in_raw(&mmap, key, index) {
+            let guard = AccessGuard {
+                mmap_ref: mmap,
+                offset,
+                len,
+            };
+            Ok(Some(guard))
+        } else {
+            Ok(None)
         }
-
-        Ok(None)
     }
 }
 
