af132e7b6ed241bbd5f24455e3c1219ff21fb1cb
Christopher Berner <christopherberner@gmail.com>
2021-07-08 18:54:43 -0700

    Implement deleting keys
    Note that we still don't support overwriting them

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,42 +1,55 @@
 use crate::error::Error;
 use crate::storage::{AccessGuard, Storage};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct WriteTransaction<'mmap> {
     storage: &'mmap Storage,
-    data: HashMap<Vec<u8>, Vec<u8>>,
+    added: HashMap<Vec<u8>, Vec<u8>>,
+    removed: HashSet<Vec<u8>>,
 }
 
 impl<'mmap> WriteTransaction<'mmap> {
     pub(in crate) fn new(storage: &'mmap Storage) -> WriteTransaction<'mmap> {
         WriteTransaction {
             storage,
-            data: HashMap::new(),
+            added: HashMap::new(),
+            removed: HashSet::new(),
         }
     }
 
     pub fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<(), Error> {
-        self.data.insert(key.to_vec(), value.to_vec());
+        self.removed.remove(key);
+        self.added.insert(key.to_vec(), value.to_vec());
         Ok(())
     }
 
     /// Reserve space to insert a key-value pair
     /// The returned reference will have length equal to value_length
     pub fn insert_reserve(&mut self, key: &[u8], value_length: usize) -> Result<&mut [u8], Error> {
-        self.data.insert(key.to_vec(), vec![0; value_length]);
-        Ok(self.data.get_mut(key).unwrap())
+        self.removed.remove(key);
+        self.added.insert(key.to_vec(), vec![0; value_length]);
+        Ok(self.added.get_mut(key).unwrap())
     }
 
     pub fn get(&self, key: &[u8]) -> Result<Option<AccessGuard>, Error> {
-        if let Some(value) = self.data.get(key) {
+        if let Some(value) = self.added.get(key) {
             return Ok(Some(AccessGuard::Local(value)));
         }
         self.storage.get(key)
     }
 
+    pub fn remove(&mut self, key: &[u8]) -> Result<(), Error> {
+        self.added.remove(key);
+        self.removed.insert(key.to_vec());
+        Ok(())
+    }
+
     pub fn commit(self) -> Result<(), Error> {
-        for (key, value) in self.data.iter() {
+        for (key, value) in self.added.iter() {
             self.storage.append(key, value)?;
+        }
+        for key in self.removed.iter() {
+            self.storage.remove(key)?;
         }
         self.storage.fsync()?;
         Ok(())
