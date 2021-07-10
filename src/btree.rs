00a125c9741e55c45b85ca6886a485265ca1a396
Christopher Berner <christopherberner@gmail.com>
2021-07-10 11:24:37 -0700

    Refactor to add accessor objects for leaf pages

--- src/btree.rs
+++ src/btree.rs
@@ -1,5 +1,5 @@
 use crate::btree::Node::{Internal, Leaf};
-use crate::page_manager::{Page, PageManager};
+use crate::page_manager::{Page, PageManager, PageMut};
 use std::cell::Cell;
 use std::cmp::Ordering;
 use std::convert::TryInto;
@@ -15,6 +15,138 @@ fn write_vec(value: &[u8], output: &mut [u8], mut index: usize) -> usize {
     index
 }
 
+// Provides a simple zero-copy way to access entries
+//
+// Entry format is:
+// * (8 bytes) key_size
+// * (key_size bytes) key_data
+// * (8 bytes) value_size
+// * (value_size bytes) value_data
+struct EntryAccessor<'a> {
+    raw: &'a [u8],
+}
+
+impl<'a> EntryAccessor<'a> {
+    fn new(raw: &'a [u8]) -> Self {
+        EntryAccessor { raw }
+    }
+
+    fn key_len(&self) -> usize {
+        u64::from_be_bytes(self.raw[0..8].try_into().unwrap()) as usize
+    }
+
+    fn key(&self) -> &'a [u8] {
+        &self.raw[8..(8 + self.key_len())]
+    }
+
+    fn value_offset(&self) -> usize {
+        8 + self.key_len() + 8
+    }
+
+    fn value_len(&self) -> usize {
+        let key_len = self.key_len();
+        u64::from_be_bytes(
+            self.raw[(8 + key_len)..(8 + key_len + 8)]
+                .try_into()
+                .unwrap(),
+        ) as usize
+    }
+
+    fn value(&self) -> &'a [u8] {
+        &self.raw[self.value_offset()..(self.value_offset() + self.value_len())]
+    }
+
+    fn raw_len(&self) -> usize {
+        8 + self.key_len() + 8 + self.value_len()
+    }
+}
+
+// Note the caller is responsible for ensuring that the buffer is large enough
+// and rewriting all fields if any dynamically sized fields are written
+struct EntryMutator<'a> {
+    raw: &'a mut [u8],
+}
+
+impl<'a> EntryMutator<'a> {
+    fn new(raw: &'a mut [u8]) -> Self {
+        EntryMutator { raw }
+    }
+
+    fn write_key(&mut self, key: &[u8]) {
+        self.raw[0..8].copy_from_slice(&(key.len() as u64).to_be_bytes());
+        self.raw[8..(8 + key.len())].copy_from_slice(key);
+    }
+
+    fn write_value(&mut self, value: &[u8]) {
+        let value_offset = 8 + EntryAccessor::new(self.raw).key_len();
+        self.raw[value_offset..(value_offset + 8)]
+            .copy_from_slice(&(value.len() as u64).to_be_bytes());
+        self.raw[(value_offset + 8)..(value_offset + 8 + value.len())].copy_from_slice(value);
+    }
+}
+
+// Provides a simple zero-copy way to access a leaf page
+//
+// Entry format is:
+// * (1 byte) type: 1 = LEAF
+// * (n bytes) lesser_entry
+// * (n bytes) greater_entry: optional
+struct LeafAccessor<'a: 'b, 'b> {
+    page: &'b Page<'a>,
+}
+
+impl<'a: 'b, 'b> LeafAccessor<'a, 'b> {
+    fn new(page: &'b Page<'a>) -> Self {
+        LeafAccessor { page }
+    }
+
+    fn offset_of_lesser(&self) -> usize {
+        1
+    }
+
+    fn offset_of_greater(&self) -> usize {
+        1 + self.lesser().raw_len()
+    }
+
+    fn lesser(&self) -> EntryAccessor {
+        EntryAccessor::new(&self.page.memory()[self.offset_of_lesser()..])
+    }
+
+    fn greater(&self) -> EntryAccessor {
+        EntryAccessor::new(&self.page.memory()[self.offset_of_greater()..])
+    }
+}
+
+// Note the caller is responsible for ensuring that the buffer is large enough
+// and rewriting all fields if any dynamically sized fields are written
+struct LeafBuilder<'a: 'b, 'b> {
+    page: &'b mut PageMut<'a>,
+}
+
+impl<'a: 'b, 'b> LeafBuilder<'a, 'b> {
+    fn new(page: &'b mut PageMut<'a>) -> Self {
+        page.memory_mut()[0] = LEAF;
+        LeafBuilder { page }
+    }
+
+    fn write_lesser(&mut self, key: &[u8], value: &[u8]) {
+        let mut entry = EntryMutator::new(&mut self.page.memory_mut()[1..]);
+        entry.write_key(key);
+        entry.write_value(value);
+    }
+
+    fn write_greater(&mut self, pair: Option<(&[u8], &[u8])>) {
+        let offset = 1 + EntryAccessor::new(&self.page.memory()[1..]).raw_len();
+        let mut entry = EntryMutator::new(&mut self.page.memory_mut()[offset..]);
+        if let Some((key, value)) = pair {
+            entry.write_key(key);
+            entry.write_value(value);
+        } else {
+            entry.write_key(&[]);
+        }
+    }
+}
+
 // Returns the (offset, len) of the value for the queried key, if present
 pub(in crate) fn lookup_in_raw<'a>(
     page: Page<'a>,
@@ -25,37 +157,20 @@ pub(in crate) fn lookup_in_raw<'a>(
     let mut index = 0;
     match node_mem[index] {
         LEAF => {
-            index += 1;
-            let key_len =
-                u64::from_be_bytes(node_mem[index..(index + 8)].try_into().unwrap()) as usize;
-            index += 8;
-            match query.cmp(&node_mem[index..(index + key_len)]) {
+            let accessor = LeafAccessor::new(&page);
+            match query.cmp(accessor.lesser().key()) {
                 Ordering::Less => None,
                 Ordering::Equal => {
-                    index += key_len;
-                    let value_len =
-                        u64::from_be_bytes(node_mem[index..(index + 8)].try_into().unwrap())
-                            as usize;
-                    index += 8;
-                    Some((page, index, value_len))
+                    let offset = accessor.offset_of_lesser() + accessor.lesser().value_offset();
+                    let value_len = accessor.lesser().value().len();
+                    Some((page, offset, value_len))
                 }
                 Ordering::Greater => {
-                    index += key_len;
-                    let value_len =
-                        u64::from_be_bytes(node_mem[index..(index + 8)].try_into().unwrap())
-                            as usize;
-                    index += 8 + value_len;
-                    let second_key_len =
-                        u64::from_be_bytes(node_mem[index..(index + 8)].try_into().unwrap())
-                            as usize;
-                    index += 8;
-                    if query == &node_mem[index..(index + second_key_len)] {
-                        index += second_key_len;
-                        let value_len =
-                            u64::from_be_bytes(node_mem[index..(index + 8)].try_into().unwrap())
-                                as usize;
-                        index += 8;
-                        Some((page, index, value_len))
+                    if query == accessor.greater().key() {
+                        let offset =
+                            accessor.offset_of_greater() + accessor.greater().value_offset();
+                        let value_len = accessor.greater().value().len();
+                        Some((page, offset, value_len))
                     } else {
                         None
                     }
@@ -94,19 +209,13 @@ impl Node {
         match self {
             Node::Leaf(left_val, right_val) => {
                 let mut page = page_manager.allocate();
-                let mut index = 0;
-                let output = page.memory_mut();
-                output[index] = LEAF;
-                index += 1;
-                index = write_vec(&left_val.0, output, index);
-                index = write_vec(&left_val.1, output, index);
-                if let Some(right) = right_val {
-                    index = write_vec(&right.0, output, index);
-                    write_vec(&right.1, output, index);
-                } else {
-                    // empty right val stored as a single 0 length key
-                    write_vec(&[], output, index);
-                }
+                let mut builder = LeafBuilder::new(&mut page);
+                builder.write_lesser(&left_val.0, &left_val.1);
+                builder.write_greater(
+                    right_val
+                        .as_ref()
+                        .map(|(key, value)| (key.as_slice(), value.as_slice())),
+                );
 
                 page.get_page_number()
             }
