4306cd10ea88a0c7f20496e23643d32a16dc8475
Christopher Berner <christopherberner@gmail.com>
2021-12-29 12:19:06 -0800

    Implement all read methods for Table

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use db::{Database, DatabaseBuilder, DatabaseTransaction, ReadOnlyDatabaseTransaction};
 pub use error::Error;
 pub use multimap_table::{MultimapRangeIter, MultimapTable, ReadOnlyMultimapTable};
-pub use table::{ReadOnlyTable, Table};
+pub use table::{RangeIter, ReadOnlyTable, ReadableTable, Table};
 pub use tree_store::AccessGuard;
 
 #[cfg(feature = "python")]
