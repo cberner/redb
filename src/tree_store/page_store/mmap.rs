4dd589e0cdd4d5d7290d4aba16b1ba8fbc9a9ba2
Christopher Berner <christopherberner@gmail.com>
2022-01-11 18:30:28 -0800

    Switch to memmap2::MmapRaw

--- src/tree_store/page_store/mmap.rs
+++ src/tree_store/page_store/mmap.rs
@@ -1,44 +1,48 @@
 use crate::Error;
-use std::cell::UnsafeCell;
-use std::ops::Range;
+use std::ops::{Deref, DerefMut, Range};
 use std::slice;
 
 pub(crate) struct Mmap {
-    inner: UnsafeCell<memmap2::MmapMut>,
-    data_ptr: *mut u8,
-    len: usize,
+    inner: memmap2::MmapRaw,
+}
+
+// TODO: remove these, they're dangerous mixed with the get_memory() methods
+impl Deref for Mmap {
+    type Target = [u8];
+
+    #[inline]
+    fn deref(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self.inner.as_ptr(), self.inner.len()) }
+    }
+}
+
+// TODO: remove these, they're dangerous mixed with the get_memory() methods
+impl DerefMut for Mmap {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut [u8] {
+        unsafe { slice::from_raw_parts_mut(self.inner.as_mut_ptr(), self.inner.len()) }
+    }
 }
 
 impl Mmap {
-    pub(crate) fn new(mut mmap: memmap2::MmapMut) -> Self {
-        let len = mmap.len();
-        let data_ptr = mmap.as_mut_ptr();
-        Self {
-            inner: UnsafeCell::new(mmap),
-            data_ptr,
-            len,
-        }
+    pub(crate) fn new(mmap: memmap2::MmapRaw) -> Self {
+        Self { inner: mmap }
     }
 
     pub(crate) fn len(&self) -> usize {
-        self.len
+        self.inner.len()
     }
 
     pub(crate) fn flush(&self) -> Result<(), Error> {
-        // Safety: .flush() does not construct any references to the memory of the mmap
-        // and the only mutable references we construct are to that memory
-        unsafe {
-            (*(self.inner.get() as *const memmap2::MmapMut)).flush()?;
-        }
+        self.inner.flush()?;
 
         Ok(())
     }
 
     pub(crate) fn get_memory(&self, range: Range<usize>) -> &[u8] {
+        assert!(range.end <= self.inner.len());
         unsafe {
-            // Avoid calling self.inner.deref() because that would create a reference to all of the
-            // mmap'ed memory
-            let ptr = (self.data_ptr as *const u8).add(range.start);
+            let ptr = self.inner.as_ptr().add(range.start);
             slice::from_raw_parts(ptr, range.len())
         }
     }
@@ -47,9 +51,8 @@ impl Mmap {
     // from .get_memory() or .get_memory_mut()
     #[allow(clippy::mut_from_ref)]
     pub(crate) unsafe fn get_memory_mut(&self, range: Range<usize>) -> &mut [u8] {
-        // Avoid calling self.inner.deref_mut() because that would create a reference to all of the
-        // mmap'ed memory, and require a mutable reference to .inner
-        let ptr = self.data_ptr.add(range.start);
+        assert!(range.end <= self.inner.len());
+        let ptr = self.inner.as_mut_ptr().add(range.start);
         slice::from_raw_parts_mut(ptr, range.len())
     }
 }
