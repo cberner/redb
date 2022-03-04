4910e4c8dc3a0639721c193b7e8cdba03cbaf647
Casey Rodarmor <casey@rodarmor.com>
2022-03-03 22:06:55 -0800

    Check formatting on CI (#206)
    * Check formatting on CI
    
    * Run clippy on CI

--- src/python.rs
+++ src/python.rs
@@ -1,7 +1,6 @@
 use pyo3::prelude::*;
-use pyo3::types::*;
 
 #[pymodule]
-pub fn redb(_py: Python, m: &PyModule) -> PyResult<()> {
+pub fn redb(_py: Python, _m: &PyModule) -> PyResult<()> {
     Ok(())
 }
