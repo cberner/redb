67686ec12d6f5ff00b666dfe49ab5180694e3161
Casey Rodarmor <casey@rodarmor.com>
2022-03-03 18:50:12 -0800

    Silence warning

--- src/python.rs
+++ src/python.rs
@@ -2,6 +2,6 @@ use pyo3::prelude::*;
 use pyo3::types::*;
 
 #[pymodule]
-pub fn redb(_py: Python, m: &PyModule) -> PyResult<()> {
+pub fn redb(_py: Python, _m: &PyModule) -> PyResult<()> {
     Ok(())
 }
