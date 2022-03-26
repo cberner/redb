cberner/redb

commit 86b0dda23f1177512e3734d70c1b4fd7c9257d4d

Switch almost all encoding to little endian

src/tree_store/btree_utils.rs
@@ -1179,7 +1179,7 @@ mod test {
         {
             let mut table = txn.open_table(X).unwrap();
             for i in (0..elements).rev() {
-                table.insert(&i.to_be_bytes(), b"").unwrap();
+                table.insert(&i.to_le_bytes(), b"").unwrap();
             }
         }
         txn.commit().unwrap();
@@ -1199,7 +1199,7 @@ mod test {
         {
             let mut table = txn.open_table(X).unwrap();
             for i in 0..(elements - reduce_to) {
-                table.remove(&i.to_be_bytes()).unwrap();
+                table.remove(&i.to_le_bytes()).unwrap();
             }
         }
         txn.commit().unwrap();

benches/lmdb_benchmark.rs
@@ -35,7 +35,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
         for _ in 0..ELEMENTS {
             let len = pairs.len();
             let (key, value) = &mut pairs[written % len];
-            key[16..].copy_from_slice(&(written as u64).to_be_bytes());
+            key[16..].copy_from_slice(&(written as u64).to_le_bytes());
             inserter.insert(key, value).unwrap();
             written += 1;
         }
@@ -61,7 +61,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
             let mut inserter = txn.get_inserter();
             let len = pairs.len();
             let (key, value) = &mut pairs[written % len];
-            key[16..].copy_from_slice(&(written as u64).to_be_bytes());
+            key[16..].copy_from_slice(&(written as u64).to_le_bytes());
             inserter.insert(key, value).unwrap();
             drop(inserter);
             txn.commit().unwrap();
@@ -88,7 +88,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
             for _ in 0..batch_size {
                 let len = pairs.len();
                 let (key, value) = &mut pairs[written % len];
-                key[16..].copy_from_slice(&(written as u64).to_be_bytes());
+                key[16..].copy_from_slice(&(written as u64).to_le_bytes());
                 inserter.insert(key, value).unwrap();
                 written += 1;
             }
@@ -120,7 +120,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
             for i in &key_order {
                 let len = pairs.len();
                 let (key, value) = &mut pairs[i % len];
-                key[16..].copy_from_slice(&(*i as u64).to_be_bytes());
+                key[16..].copy_from_slice(&(*i as u64).to_le_bytes());
                 let result = txn.get(key).unwrap();
                 checksum += result.as_ref()[0] as u64;
                 expected_checksum += value[0] as u64;
@@ -142,7 +142,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
             for i in &key_order {
                 let len = pairs.len();
                 let (key, _) = &mut pairs[i % len];
-                key[16..].copy_from_slice(&(*i as u64).to_be_bytes());
+                key[16..].copy_from_slice(&(*i as u64).to_le_bytes());
                 txn.exists_after(key);
             }
             let end = Instant::now();
@@ -165,7 +165,7 @@ fn benchmark<T: BenchDatabase>(mut db: T) -> Vec<(&'static str, Duration)> {
         for i in 0..deletes {
             let len = pairs.len();
             let (key, _) = &mut pairs[i % len];
-            key[16..].copy_from_slice(&(i as u64).to_be_bytes());
+            key[16..].copy_from_slice(&(i as u64).to_le_bytes());
             inserter.remove(key).unwrap();
         }
         drop(inserter);

benches/syscall_benchmark.rs
@@ -77,7 +77,7 @@ fn lmdb_zero_bench(path: &str) {
             for i in 0..ELEMENTS {
                 let (key, value) = &pairs[i % pairs.len()];
                 let mut mut_key = key.clone();
-                mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+                mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
                 access
                     .put(&db, &mut_key, value, lmdb_zero::put::Flags::empty())
                     .unwrap();
@@ -102,7 +102,7 @@ fn lmdb_zero_bench(path: &str) {
                 for &i in &key_order {
                     let (key, value) = &pairs[i % pairs.len()];
                     let mut mut_key = key.clone();
-                    mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+                    mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
                     let result: &[u8] = access.get(&db, &mut_key).unwrap();
                     checksum += result[0] as u64;
                     expected_checksum += value[0] as u64;
@@ -136,7 +136,7 @@ fn uring_bench(path: &Path) {
         for i in 0..ELEMENTS {
             let (key, value) = &pairs[i % pairs.len()];
             let mut mut_key = key.clone();
-            mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+            mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
             file.write_all(&mut_key).unwrap();
             file.write_all(value).unwrap();
         }
@@ -163,7 +163,7 @@ fn uring_bench(path: &Path) {
             for (uring_counter, &i) in key_order.iter().enumerate() {
                 let (key, value) = &pairs[i % pairs.len()];
                 let mut mut_key = key.clone();
-                mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+                mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
                 let offset = i * (mut_key.len() + value.len()) + mut_key.len();
 
                 expected_checksum += value[0] as u64;
@@ -220,7 +220,7 @@ fn readwrite_bench(path: &Path) {
         for i in 0..ELEMENTS {
             let (key, value) = &pairs[i % pairs.len()];
             let mut mut_key = key.clone();
-            mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+            mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
             file.write_all(&mut_key).unwrap();
             file.write_all(value).unwrap();
         }
@@ -243,7 +243,7 @@ fn readwrite_bench(path: &Path) {
             for &i in &key_order {
                 let (key, value) = &pairs[i % pairs.len()];
                 let mut mut_key = key.clone();
-                mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+                mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
                 let offset = i * (mut_key.len() + value.len()) + mut_key.len();
 
                 file.seek(SeekFrom::Start(offset as u64)).unwrap();
@@ -284,7 +284,7 @@ fn mmap_bench(path: &Path) {
         for i in 0..ELEMENTS {
             let (key, value) = &pairs[i % pairs.len()];
             let mut mut_key = key.clone();
-            mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+            mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
             mmap[write_index..(write_index + mut_key.len())].copy_from_slice(&mut_key);
             write_index += mut_key.len();
             mmap[write_index..(write_index + value.len())].copy_from_slice(value);
@@ -308,7 +308,7 @@ fn mmap_bench(path: &Path) {
             for &i in &key_order {
                 let (key, value) = &pairs[i % pairs.len()];
                 let mut mut_key = key.clone();
-                mut_key[0..8].copy_from_slice(&(i as u64).to_be_bytes());
+                mut_key[0..8].copy_from_slice(&(i as u64).to_le_bytes());
                 let offset = i * (mut_key.len() + value.len()) + mut_key.len();
                 let buffer = &mmap[offset..(offset + value.len())];
                 checksum += buffer[0] as u64;

src/multimap_table.rs
@@ -148,7 +148,7 @@ impl<'a, K: RedbKey + ?Sized + 'a, V: RedbKey + ?Sized + 'a> MultimapKVPairAcces
     }
 
     fn key_len(&self) -> usize {
-        u32::from_be_bytes(self.data[1..5].try_into().unwrap()) as usize
+        u32::from_le_bytes(self.data[1..5].try_into().unwrap()) as usize
     }
 
     fn key_bytes(&self) -> &'a [u8] {
@@ -162,7 +162,7 @@ impl<'a, K: RedbKey + ?Sized + 'a, V: RedbKey + ?Sized + 'a> MultimapKVPairAcces
 
 fn make_serialized_kv<K: RedbKey + ?Sized, V: RedbKey + ?Sized>(key: &K, value: &V) -> Vec<u8> {
     let mut result = vec![MultimapKeyCompareOp::KeyAndValue.serialize()];
-    result.extend_from_slice(&(key.as_bytes().as_ref().len() as u32).to_be_bytes());
+    result.extend_from_slice(&(key.as_bytes().as_ref().len() as u32).to_le_bytes());
     result.extend_from_slice(key.as_bytes().as_ref());
     result.extend_from_slice(value.as_bytes().as_ref());
 
@@ -171,7 +171,7 @@ fn make_serialized_kv<K: RedbKey + ?Sized, V: RedbKey + ?Sized>(key: &K, value: 
 
 fn make_serialized_key_with_op<K: RedbKey + ?Sized>(key: &K, op: MultimapKeyCompareOp) -> Vec<u8> {
     let mut result = vec![op.serialize()];
-    result.extend_from_slice(&(key.as_bytes().as_ref().len() as u32).to_be_bytes());
+    result.extend_from_slice(&(key.as_bytes().as_ref().len() as u32).to_le_bytes());
     result.extend_from_slice(key.as_bytes().as_ref());
 
     result

src/tree_store/btree_base.rs
@@ -165,7 +165,7 @@ impl<'a: 'b, 'b, T: Page + 'a> LeafAccessor<'a, 'b, T> {
             None
         } else {
             let offset = 4 + size_of::<u32>() * n;
-            let end = u32::from_be_bytes(
+            let end = u32::from_le_bytes(
                 self.page.memory()[offset..(offset + size_of::<u32>())]
                     .try_into()
                     .unwrap(),
@@ -187,7 +187,7 @@ impl<'a: 'b, 'b, T: Page + 'a> LeafAccessor<'a, 'b, T> {
             None
         } else {
             let offset = 4 + size_of::<u32>() * self.num_pairs() + size_of::<u32>() * n;
-            let end = u32::from_be_bytes(
+            let end = u32::from_le_bytes(
                 self.page.memory()[offset..(offset + size_of::<u32>())]
                     .try_into()
                     .unwrap(),
@@ -197,7 +197,7 @@ impl<'a: 'b, 'b, T: Page + 'a> LeafAccessor<'a, 'b, T> {
     }
 
     pub(in crate::tree_store) fn num_pairs(&self) -> usize {
-        u16::from_be_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
+        u16::from_le_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
     }
 
     pub(in crate::tree_store) fn offset_of_first_value(&self) -> usize {
@@ -301,7 +301,7 @@ impl<'a: 'b, 'b> LeafBuilder<'a, 'b> {
         key_bytes: usize,
     ) -> Self {
         page.memory_mut()[0] = LEAF;
-        page.memory_mut()[2..4].copy_from_slice(&(num_pairs as u16).to_be_bytes());
+        page.memory_mut()[2..4].copy_from_slice(&(num_pairs as u16).to_le_bytes());
         #[cfg(debug_assertions)]
         {
             // Poison all the key & value offsets, in case the caller forgets to write them
@@ -320,7 +320,7 @@ impl<'a: 'b, 'b> LeafBuilder<'a, 'b> {
 
     fn value_end(&self, n: usize) -> usize {
         let offset = 4 + size_of::<u32>() * self.num_pairs + size_of::<u32>() * n;
-        u32::from_be_bytes(
+        u32::from_le_bytes(
             self.page.memory()[offset..(offset + size_of::<u32>())]
                 .try_into()
                 .unwrap(),
@@ -329,7 +329,7 @@ impl<'a: 'b, 'b> LeafBuilder<'a, 'b> {
 
     fn key_end(&self, n: usize) -> usize {
         let offset = 4 + size_of::<u32>() * n;
-        u32::from_be_bytes(
+        u32::from_le_bytes(
             self.page.memory()[offset..(offset + size_of::<u32>())]
                 .try_into()
                 .unwrap(),
@@ -351,14 +351,14 @@ impl<'a: 'b, 'b> LeafBuilder<'a, 'b> {
         let n = self.pairs_written;
         let offset = 4 + size_of::<u32>() * n;
         self.page.memory_mut()[offset..(offset + size_of::<u32>())]
-            .copy_from_slice(&((key_offset + key.len()) as u32).to_be_bytes());
+            .copy_from_slice(&((key_offset + key.len()) as u32).to_le_bytes());
         self.page.memory_mut()[key_offset..(key_offset + key.len())].copy_from_slice(key);
         let written_key_len = key_offset + key.len() - 4 - 2 * size_of::<u32>() * self.num_pairs;
         assert!(written_key_len <= self.provisioned_key_bytes);
 
         let offset = 4 + size_of::<u32>() * self.num_pairs + size_of::<u32>() * n;
         self.page.memory_mut()[offset..(offset + size_of::<u32>())]
-            .copy_from_slice(&((value_offset + value.len()) as u32).to_be_bytes());
+            .copy_from_slice(&((value_offset + value.len()) as u32).to_le_bytes());
         self.page.memory_mut()[value_offset..(value_offset + value.len())].copy_from_slice(value);
         self.pairs_written += 1;
     }
@@ -427,7 +427,7 @@ impl<'a: 'b, 'b, T: Page + 'a> InternalAccessor<'a, 'b, T> {
     fn key_end(&self, n: usize) -> usize {
         let offset =
             4 + PageNumber::serialized_size() * self.count_children() + size_of::<u32>() * n;
-        u32::from_be_bytes(
+        u32::from_le_bytes(
             self.page.memory()[offset..(offset + size_of::<u32>())]
                 .try_into()
                 .unwrap(),
@@ -455,7 +455,7 @@ impl<'a: 'b, 'b, T: Page + 'a> InternalAccessor<'a, 'b, T> {
         }
 
         let offset = 4 + PageNumber::serialized_size() * n;
-        Some(PageNumber::from_be_bytes(
+        Some(PageNumber::from_le_bytes(
             self.page.memory()[offset..(offset + PageNumber::serialized_size())]
                 .try_into()
                 .unwrap(),
@@ -467,7 +467,7 @@ impl<'a: 'b, 'b, T: Page + 'a> InternalAccessor<'a, 'b, T> {
     }
 
     fn num_keys(&self) -> usize {
-        u16::from_be_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
+        u16::from_le_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
     }
 }
 
@@ -499,7 +499,7 @@ impl<'a: 'b, 'b> InternalBuilder<'a, 'b> {
     // Caller MUST write num_keys values
     pub(in crate::tree_store) fn new(page: &'b mut PageMut<'a>, num_keys: usize) -> Self {
         page.memory_mut()[0] = INTERNAL;
-        page.memory_mut()[2..4].copy_from_slice(&(num_keys as u16).to_be_bytes());
+        page.memory_mut()[2..4].copy_from_slice(&(num_keys as u16).to_le_bytes());
         #[cfg(debug_assertions)]
         {
             // Poison all the child pointers & key offsets, in case the caller forgets to write them
@@ -519,12 +519,12 @@ impl<'a: 'b, 'b> InternalBuilder<'a, 'b> {
     pub(in crate::tree_store) fn write_first_page(&mut self, page_number: PageNumber) {
         let offset = 4;
         self.page.memory_mut()[offset..(offset + PageNumber::serialized_size())]
-            .copy_from_slice(&page_number.to_be_bytes());
+            .copy_from_slice(&page_number.to_le_bytes());
     }
 
     fn key_end(&self, n: usize) -> usize {
         let offset = 4 + PageNumber::serialized_size() * (self.num_keys + 1) + size_of::<u32>() * n;
-        u32::from_be_bytes(
+        u32::from_le_bytes(
             self.page.memory()[offset..(offset + size_of::<u32>())]
                 .try_into()
                 .unwrap(),
@@ -544,7 +544,7 @@ impl<'a: 'b, 'b> InternalBuilder<'a, 'b> {
         self.keys_written += 1;
         let offset = 4 + PageNumber::serialized_size() * (n + 1);
         self.page.memory_mut()[offset..(offset + PageNumber::serialized_size())]
-            .copy_from_slice(&page_number.to_be_bytes());
+            .copy_from_slice(&page_number.to_le_bytes());
 
         let data_offset = if n > 0 {
             self.key_end(n - 1)
@@ -554,7 +554,7 @@ impl<'a: 'b, 'b> InternalBuilder<'a, 'b> {
         };
         let offset = 4 + PageNumber::serialized_size() * (self.num_keys + 1) + size_of::<u32>() * n;
         self.page.memory_mut()[offset..(offset + size_of::<u32>())]
-            .copy_from_slice(&((data_offset + key.len()) as u32).to_be_bytes());
+            .copy_from_slice(&((data_offset + key.len()) as u32).to_le_bytes());
 
         debug_assert!(data_offset > offset);
         self.page.memory_mut()[data_offset..(data_offset + key.len())].copy_from_slice(key);
@@ -578,13 +578,13 @@ impl<'a: 'b, 'b> InternalMutator<'a, 'b> {
     }
 
     fn num_keys(&self) -> usize {
-        u16::from_be_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
+        u16::from_le_bytes(self.page.memory()[2..4].try_into().unwrap()) as usize
     }
 
     pub(in crate::tree_store) fn write_child_page(&mut self, i: usize, page_number: PageNumber) {
         debug_assert!(i <= self.num_keys());
         let offset = 4 + PageNumber::serialized_size() * i;
         self.page.memory_mut()[offset..(offset + PageNumber::serialized_size())]
-            .copy_from_slice(&page_number.to_be_bytes());
+            .copy_from_slice(&page_number.to_le_bytes());
     }
 }

src/tree_store/page_store/buddy_allocator.rs
@@ -15,7 +15,7 @@ pub(crate) struct BuddyAllocator {
 // Pages are marked free at only a single order, and it must always be the largest order
 //
 // Data structure format:
-// max_order: big endian u64
+// max_order: u64
 // order_offsets: array of (u64, u64), with offset & length for PageAllocator structure for the given order
 // ... PageAllocator structures
 impl BuddyAllocator {
@@ -41,7 +41,7 @@ impl BuddyAllocator {
         max_order: usize,
     ) -> Self {
         assert!(data.len() >= Self::required_space(max_page_capacity, max_order));
-        data[..size_of::<u64>()].copy_from_slice(&(max_order as u64).to_be_bytes());
+        data[..size_of::<u64>()].copy_from_slice(&(max_order as u64).to_le_bytes());
 
         let mut metadata_offset = size_of::<u64>();
         let mut data_offset = size_of::<u64>() + 2 * (max_order + 1) * size_of::<u64>();
@@ -51,9 +51,9 @@ impl BuddyAllocator {
         for order in 0..=max_order {
             let required = PageAllocator::required_space(pages_for_order);
             data[metadata_offset..metadata_offset + size_of::<u64>()]
-                .copy_from_slice(&(data_offset as u64).to_be_bytes());
+                .copy_from_slice(&(data_offset as u64).to_le_bytes());
             data[metadata_offset + size_of::<u64>()..metadata_offset + 2 * size_of::<u64>()]
-                .copy_from_slice(&(required as u64).to_be_bytes());
+                .copy_from_slice(&(required as u64).to_le_bytes());
             orders.push(PageAllocator::init_new(
                 Self::get_order_bytes_mut(data, order),
                 pages_for_order,
@@ -135,12 +135,12 @@ impl BuddyAllocator {
     }
 
     fn get_order_offset_and_length(data: &[u8], order: usize) -> (usize, usize) {
-        let max_order = u64::from_be_bytes(data[..size_of::<u64>()].try_into().unwrap()) as usize;
+        let max_order = u64::from_le_bytes(data[..size_of::<u64>()].try_into().unwrap()) as usize;
         assert!(order <= max_order);
         let base = size_of::<u64>() + order * 2 * size_of::<u64>();
         let offset =
-            u64::from_be_bytes(data[base..base + size_of::<u64>()].try_into().unwrap()) as usize;
-        let length = u64::from_be_bytes(
+            u64::from_le_bytes(data[base..base + size_of::<u64>()].try_into().unwrap()) as usize;
+        let length = u64::from_le_bytes(
             data[base + size_of::<u64>()..base + 2 * size_of::<u64>()]
                 .try_into()
                 .unwrap(),

src/tree_store/page_store/grouped_bitmap.rs
@@ -39,24 +39,24 @@ impl<'a> U64GroupedBitMapMut<'a> {
     // Returns true iff the bit's group is all set
     pub(crate) fn set(&mut self, bit: usize) -> bool {
         let (index, bit_index) = self.data_index_of(bit);
-        let mut group = u64::from_be_bytes(self.data[index..(index + 8)].try_into().unwrap());
+        let mut group = u64::from_le_bytes(self.data[index..(index + 8)].try_into().unwrap());
         group |= Self::select_mask(bit_index);
-        self.data[index..(index + 8)].copy_from_slice(&group.to_be_bytes());
+        self.data[index..(index + 8)].copy_from_slice(&group.to_le_bytes());
 
         group == u64::MAX
     }
 
     pub(crate) fn get(&self, bit: usize) -> bool {
         let (index, bit_index) = self.data_index_of(bit);
-        let group = u64::from_be_bytes(self.data[index..(index + 8)].try_into().unwrap());
+        let group = u64::from_le_bytes(self.data[index..(index + 8)].try_into().unwrap());
         group & Self::select_mask(bit_index) != 0
     }
 
     pub(crate) fn clear(&mut self, bit: usize) {
         let (index, bit_index) = self.data_index_of(bit);
-        let mut group = u64::from_be_bytes(self.data[index..(index + 8)].try_into().unwrap());
+        let mut group = u64::from_le_bytes(self.data[index..(index + 8)].try_into().unwrap());
         group &= !Self::select_mask(bit_index);
-        self.data[index..(index + 8)].copy_from_slice(&group.to_be_bytes());
+        self.data[index..(index + 8)].copy_from_slice(&group.to_le_bytes());
     }
 
     pub(crate) fn first_unset(&self, start_bit: usize, end_bit: usize) -> Option<usize> {
@@ -64,7 +64,7 @@ impl<'a> U64GroupedBitMapMut<'a> {
         assert_eq!(end_bit, start_bit + 64);
 
         let (index, _) = self.data_index_of(start_bit);
-        let group = u64::from_be_bytes(self.data[index..(index + 8)].try_into().unwrap());
+        let group = u64::from_le_bytes(self.data[index..(index + 8)].try_into().unwrap());
         match group.trailing_ones() {
             64 => None,
             x => Some(start_bit + x as usize),

src/tree_store/page_store/page_allocator.rs
@@ -14,7 +14,7 @@ pub(crate) struct PageAllocator {
 // borrowing the data array
 //
 // Data structure format:
-// num_pages: big endian u64
+// num_pages: u64
 // root: u64
 // subtree layer: 2-64 u64s
 // ...consecutive layers. Except for the last level, all sub-trees of the root must be complete
@@ -67,7 +67,7 @@ impl PageAllocator {
             *value = 0xFF;
         }
 
-        data[..8].copy_from_slice(&(num_pages as u64).to_be_bytes());
+        data[..8].copy_from_slice(&(num_pages as u64).to_le_bytes());
 
         let result = Self::new(num_pages);
 
@@ -323,7 +323,7 @@ mod test {
         while allocator.alloc(&mut data).is_ok() {}
         // The last u64 must be used, since the leaf layer is compact
         let l = data.len();
-        assert_ne!(0, u64::from_be_bytes(data[(l - 8)..].try_into().unwrap()));
+        assert_ne!(0, u64::from_le_bytes(data[(l - 8)..].try_into().unwrap()));
     }
 
     #[test]

commit 9c6373d22d8f7c4abe9b628008a571896145e7db

Fix regression
Fix the sorting of children when merging or splitting an index page.
This regression was introduced by
86b0dda23f1177512e3734d70c1b4fd7c9257d4d, but was a long standing bug
where the RedbKey sort order was not used

src/tree_store/btree_utils.rs
@@ -372,7 +372,7 @@ unsafe fn merge_leaf<K: RedbKey + ?Sized>(
 // Splits the page, if necessary, to fit the additional pages in `partial`
 // Returns the pages in order
 // Safety: caller must ensure that no references to uncommitted pages in this table exist
-unsafe fn split_index(
+unsafe fn split_index<K: RedbKey + ?Sized>(
     index: PageNumber,
     partial: &[PageNumber],
     manager: &TransactionalMemory,
@@ -393,7 +393,11 @@ unsafe fn split_index(
         pages.push(child);
     }
 
-    pages.sort_by_key(|p| max_key(manager.get_page(*p), manager));
+    pages.sort_by(|p1, p2| {
+        let k1 = max_key(manager.get_page(*p1), manager);
+        let k2 = max_key(manager.get_page(*p2), manager);
+        K::compare(&k1, &k2)
+    });
 
     let division = pages.len() / 2;
 
@@ -434,7 +438,7 @@ fn make_index_many_pages(
 }
 
 // Safety: caller must ensure that no references to uncommitted pages in this table exist
-unsafe fn merge_index(
+unsafe fn merge_index<K: RedbKey + ?Sized>(
     index: PageNumber,
     partial: &[PageNumber],
     manager: &TransactionalMemory,
@@ -452,7 +456,11 @@ unsafe fn merge_index(
         pages.push(child);
     }
 
-    pages.sort_by_key(|p| max_key(manager.get_page(*p), manager));
+    pages.sort_by(|p1, p2| {
+        let k1 = max_key(manager.get_page(*p1), manager);
+        let k2 = max_key(manager.get_page(*p2), manager);
+        K::compare(&k1, &k2)
+    });
     assert!(pages.len() <= BTREE_ORDER);
 
     drop(page);
@@ -656,7 +664,7 @@ unsafe fn tree_delete_helper<'a, K: RedbKey + ?Sized, V: RedbValue + ?Sized>(
                         }
                         let page_number = accessor.child_page(i).unwrap();
                         if i == merge_with {
-                            if let Some((page1, page2)) = split_index(
+                            if let Some((page1, page2)) = split_index::<K>(
                                 page_number,
                                 &partials,
                                 manager,
@@ -666,7 +674,7 @@ unsafe fn tree_delete_helper<'a, K: RedbKey + ?Sized, V: RedbValue + ?Sized>(
                                 children.push(page1);
                                 children.push(page2);
                             } else {
-                                children.push(merge_index(
+                                children.push(merge_index::<K>(
                                     page_number,
                                     &partials,
                                     manager,
@@ -1144,11 +1152,38 @@ pub(crate) fn find_key<'a, K: RedbKey + ?Sized, V: RedbValue + ?Sized>(
 
 #[cfg(test)]
 mod test {
-    use crate::tree_store::btree_utils::BTREE_ORDER;
+    use crate::tree_store::btree_base::InternalAccessor;
+    use crate::tree_store::btree_utils::{
+        make_index_many_pages, make_mut_single_leaf, merge_index, BTREE_ORDER,
+    };
+    use crate::tree_store::page_store::TransactionalMemory;
     use crate::{Database, TableDefinition};
     use tempfile::NamedTempFile;
 
     const X: TableDefinition<[u8], [u8]> = TableDefinition::new("x");
+
+    #[test]
+    // Test for regression in index merge/split code found by fuzzer, where keys were sorted
+    // in their binary order rather than by their RedbKey::compare order
+    fn merge_regression() {
+        let tmpfile: NamedTempFile = NamedTempFile::new().unwrap();
+        let file = tmpfile.into_file();
+        let mem = TransactionalMemory::new(file, 1024 * 1024, None, true).unwrap();
+        let (one, _) = make_mut_single_leaf(&1u64.to_le_bytes(), &[], &mem).unwrap();
+        let (two, _) = make_mut_single_leaf(&2u64.to_le_bytes(), &[], &mem).unwrap();
+        // a value which has the second byte set, but zero in the first byte
+        let (next_byte, _) = make_mut_single_leaf(&256u64.to_le_bytes(), &[], &mem).unwrap();
+        let index = make_index_many_pages(&[one, two], &mem).unwrap();
+        let mut freed = vec![];
+        let merged =
+            unsafe { merge_index::<u64>(index, &[next_byte], &mem, false, &mut freed) }.unwrap();
+        let merged_page = mem.get_page(merged);
+        let accessor = InternalAccessor::new(&merged_page);
+        assert_eq!(accessor.count_children(), 3);
+        assert_eq!(accessor.child_page(0).unwrap(), one);
+        assert_eq!(accessor.child_page(1).unwrap(), two);
+        assert_eq!(accessor.child_page(2).unwrap(), next_byte);
+    }
 
     #[test]
     fn tree_balance() {