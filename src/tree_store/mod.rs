b24809aae78f03af411318cfdbc499bfbee5aec1
Christopher Berner <christopherberner@gmail.com>
2022-04-01 17:57:57 -0700

    Various minor cleanups

--- src/tree_store/mod.rs
+++ src/tree_store/mod.rs
@@ -8,9 +8,7 @@ mod table_tree;
 pub(crate) use btree::{Btree, BtreeMut};
 pub use btree_base::AccessGuard;
 pub(crate) use btree_base::AccessGuardMut;
-pub(crate) use btree_iters::{
-    page_numbers_iter_start_state, AllPageNumbersBtreeIter, BtreeRangeIter,
-};
+pub(crate) use btree_iters::{AllPageNumbersBtreeIter, BtreeRangeIter};
 pub(crate) use page_store::{get_db_size, PageNumber, TransactionalMemory};
 pub(crate) use table_tree::{
     FreedTableKey, InternalTableDefinition, TableTree, TableType, FREED_TABLE,
