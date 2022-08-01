099927b411074ec9b6b43f1ad642e4b16917a9d7
Christopher Berner <christopherberner@gmail.com>
2022-07-31 20:02:00 -0700

    Be more paranoid about fsync failures

--- src/tree_store/page_store/mmap.rs
+++ src/tree_store/page_store/mmap.rs
@@ -1,9 +1,10 @@
 use crate::{Error, Result};
 use std::fs::File;
 use std::io;
+use std::io::ErrorKind;
 use std::ops::Range;
 use std::os::unix::io::AsRawFd;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::{ptr, slice};
 
 struct FileLock {
@@ -37,6 +38,7 @@ pub(crate) struct Mmap {
     _lock: FileLock,
     mmap: *mut u8,
     len: AtomicUsize,
+    fsync_failed: AtomicBool,
     capacity: usize,
 }
 
@@ -62,13 +64,27 @@ impl Mmap {
         if mmap == libc::MAP_FAILED {
             return Err(io::Error::last_os_error().into());
         }
-        Ok(Self {
+
+        let result = Self {
             mmap: mmap as *mut u8,
             file,
             _lock: lock,
             len: AtomicUsize::new(len as usize),
+            fsync_failed: AtomicBool::new(false),
             capacity: max_capacity,
-        })
+        };
+        // fsync to ensure that the data we read during initialization is stored durably
+        result.flush()?;
+
+        Ok(result)
+    }
+
+    fn check_fsync_failure(&self) -> Result<()> {
+        if self.fsync_failed.load(Ordering::Acquire) {
+            Err(Error::Io(io::Error::from(ErrorKind::Other)))
+        } else {
+            Ok(())
+        }
     }
 
     pub(crate) fn len(&self) -> usize {
@@ -78,6 +94,7 @@ impl Mmap {
     // Safety: if new_len < len(), caller must ensure that no references to memory in new_len..len() exist
     pub(crate) unsafe fn resize(&self, new_len: usize) -> Result<()> {
         assert!(new_len <= self.capacity);
+        self.check_fsync_failure()?;
         self.file.set_len(new_len as u64)?;
 
         let mmap = libc::mmap(
@@ -100,6 +117,7 @@ impl Mmap {
 
     #[cfg(not(target_os = "macos"))]
     pub(crate) fn flush(&self) -> Result {
+        self.check_fsync_failure()?;
         // Disable fsync when fuzzing, since it doesn't test crash consistency
         #[cfg(not(fuzzing))]
         {
@@ -111,6 +129,7 @@ impl Mmap {
                 )
             };
             if result != 0 {
+                self.fsync_failed.store(true, Ordering::Release);
                 return Err(io::Error::last_os_error().into());
             }
         }
@@ -119,10 +138,12 @@ impl Mmap {
 
     #[cfg(target_os = "macos")]
     pub(crate) fn flush(&self) -> Result {
+        self.check_fsync_failure()?;
         #[cfg(not(fuzzing))]
         {
             let code = unsafe { libc::fcntl(self.file.as_raw_fd(), libc::F_FULLFSYNC) };
             if code == -1 {
+                self.fsync_failed.store(true, Ordering::Release);
                 return Err(io::Error::last_os_error().into());
             }
         }
@@ -131,17 +152,20 @@ impl Mmap {
 
     #[cfg(not(target_os = "macos"))]
     pub(crate) fn eventual_flush(&self) -> Result {
+        self.check_fsync_failure()?;
         self.flush()
     }
 
     #[cfg(target_os = "macos")]
     pub(crate) fn eventual_flush(&self) -> Result {
+        self.check_fsync_failure()?;
         // TODO: It may be unsafe to mix F_BARRIERFSYNC with writes to the mmap.
         //       Investigate switching to `write()`
         #[cfg(not(fuzzing))]
         {
             let code = unsafe { libc::fcntl(self.file.as_raw_fd(), libc::F_BARRIERFSYNC) };
             if code == -1 {
+                self.fsync_failed.store(true, Ordering::Release);
                 return Err(io::Error::last_os_error().into());
             }
         }
@@ -152,6 +176,9 @@ impl Mmap {
     // from .get_memory_mut()
     pub(crate) unsafe fn get_memory(&self, range: Range<usize>) -> &[u8] {
         assert!(range.end <= self.len());
+        // TODO: propagate the error
+        self.check_fsync_failure()
+            .expect("fsync previously failed. Connection closed");
         let ptr = self.mmap.add(range.start);
         slice::from_raw_parts(ptr, range.len())
     }
@@ -161,6 +188,9 @@ impl Mmap {
     #[allow(clippy::mut_from_ref)]
     pub(crate) unsafe fn get_memory_mut(&self, range: Range<usize>) -> &mut [u8] {
         assert!(range.end <= self.len());
+        // TODO: propagate the error
+        self.check_fsync_failure()
+            .expect("fsync previously failed. Connection closed");
         let ptr = self.mmap.add(range.start);
         slice::from_raw_parts_mut(ptr, range.len())
     }
