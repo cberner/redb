205599829e5bb9200d75ff490d90b946b2364249
Jake Shadle <jake.shadle@embark-studios.com>
2022-09-23 19:12:50 +0200

    Apparently dbg! makes CI mad

--- src/db.rs
+++ src/db.rs
@@ -592,7 +592,7 @@ impl Builder {
     }
 }
 
-// This just makes it easier to throw dbg! etc statements on `Result<Database>`
+// This just makes it easier to throw `dbg` etc statements on `Result<Database>`
 impl std::fmt::Debug for Database {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Database").finish()
