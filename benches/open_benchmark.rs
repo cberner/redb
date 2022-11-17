ae33dd58174cdc5d879fae33a39d0f05a4a36c1a
Casey Rodarmor <casey@rodarmor.com>
2022-11-17 14:46:56 -0800

    Remove operations other than load

--- benches/open_benchmark.rs
+++ benches/open_benchmark.rs
@@ -9,7 +9,6 @@ use common::*;
 use redb::WriteStrategy;
 use std::time::{Duration, Instant};
 
-const ITERATIONS: usize = 3;
 const ELEMENTS: usize = 1_000_000;
 const KEY_SIZE: usize = 24;
 const VALUE_SIZE: usize = 150;
@@ -56,7 +55,7 @@ fn make_rng() -> fastrand::Rng {
     fastrand::Rng::with_seed(RNG_SEED)
 }
 
-fn benchmark<T: BenchDatabase>(db: T) -> Vec<(&'static str, Duration)> {
+fn populate<T: BenchDatabase>(db: T) -> Vec<(&'static str, Duration)> {
     let mut rng = make_rng();
     let mut results = Vec::new();
 
@@ -82,136 +81,6 @@ fn benchmark<T: BenchDatabase>(db: T) -> Vec<(&'static str, Duration)> {
     );
     results.push(("bulk load", duration));
 
-    let start = Instant::now();
-    let writes = 100;
-    {
-        for _ in 0..writes {
-            let mut txn = db.write_transaction();
-            let mut inserter = txn.get_inserter();
-            let (key, value) = gen_pair(&mut rng);
-            inserter.insert(&key, &value).unwrap();
-            drop(inserter);
-            txn.commit().unwrap();
-        }
-    }
-
-    let end = Instant::now();
-    let duration = end - start;
-    println!(
-        "{}: Wrote {} individual items in {}ms",
-        T::db_type_name(),
-        writes,
-        duration.as_millis()
-    );
-    results.push(("individual writes", duration));
-
-    let start = Instant::now();
-    let batch_size = 1000;
-    {
-        for _ in 0..writes {
-            let mut txn = db.write_transaction();
-            let mut inserter = txn.get_inserter();
-            for _ in 0..batch_size {
-                let (key, value) = gen_pair(&mut rng);
-                inserter.insert(&key, &value).unwrap();
-            }
-            drop(inserter);
-            txn.commit().unwrap();
-        }
-    }
-
-    let end = Instant::now();
-    let duration = end - start;
-    println!(
-        "{}: Wrote {} x {} items in {}ms",
-        T::db_type_name(),
-        writes,
-        batch_size,
-        duration.as_millis()
-    );
-    results.push(("batch writes", duration));
-
-    let txn = db.read_transaction();
-    {
-        for _ in 0..ITERATIONS {
-            let mut rng = make_rng();
-            let start = Instant::now();
-            let mut checksum = 0u64;
-            let mut expected_checksum = 0u64;
-            let reader = txn.get_reader();
-            for _ in 0..ELEMENTS {
-                let (key, value) = gen_pair(&mut rng);
-                let result = reader.get(&key).unwrap();
-                checksum += result.as_ref()[0] as u64;
-                expected_checksum += value[0] as u64;
-            }
-            assert_eq!(checksum, expected_checksum);
-            let end = Instant::now();
-            let duration = end - start;
-            println!(
-                "{}: Random read {} items in {}ms",
-                T::db_type_name(),
-                ELEMENTS,
-                duration.as_millis()
-            );
-            results.push(("random reads", duration));
-        }
-
-        for _ in 0..ITERATIONS {
-            let mut rng = make_rng();
-            let start = Instant::now();
-            let reader = txn.get_reader();
-            let mut value_sum = 0;
-            let num_scan = 10;
-            for _ in 0..ELEMENTS {
-                let (key, _value) = gen_pair(&mut rng);
-                let mut iter = reader.range_from(&key);
-                for _ in 0..num_scan {
-                    if let Some((_, value)) = iter.next() {
-                        value_sum += value.as_ref()[0];
-                    } else {
-                        break;
-                    }
-                }
-            }
-            assert!(value_sum > 0);
-            let end = Instant::now();
-            let duration = end - start;
-            println!(
-                "{}: Random range read {} elements in {}ms",
-                T::db_type_name(),
-                ELEMENTS * num_scan,
-                duration.as_millis()
-            );
-            results.push(("random range reads", duration));
-        }
-    }
-    drop(txn);
-
-    let start = Instant::now();
-    let deletes = ELEMENTS / 2;
-    {
-        let mut rng = make_rng();
-        let mut txn = db.write_transaction();
-        let mut inserter = txn.get_inserter();
-        for _ in 0..deletes {
-            let (key, _value) = gen_pair(&mut rng);
-            inserter.remove(&key).unwrap();
-        }
-        drop(inserter);
-        txn.commit().unwrap();
-    }
-
-    let end = Instant::now();
-    let duration = end - start;
-    println!(
-        "{}: Removed {} items in {}ms",
-        T::db_type_name(),
-        deletes,
-        duration.as_millis()
-    );
-    results.push(("removals", duration));
-
     results
 }
 
@@ -225,7 +94,7 @@ fn main() {
                 .unwrap()
         };
         let table = RedbBenchDatabase::new(&db);
-        benchmark(table)
+        populate(table)
     };
 
     let redb_throughput_results = {
@@ -237,7 +106,7 @@ fn main() {
                 .unwrap()
         };
         let table = RedbBenchDatabase::new(&db);
-        benchmark(table)
+        populate(table)
     };
 
     let lmdb_results = {
@@ -245,21 +114,21 @@ fn main() {
         let env = lmdb::Environment::new().open(tmpfile.path()).unwrap();
         env.set_map_size(4096 * 1024 * 1024).unwrap();
         let table = LmdbRkvBenchDatabase::new(&env);
-        benchmark(table)
+        populate(table)
     };
 
     let rocksdb_results = {
         let tmpfile: TempDir = tempfile::tempdir_in(current_dir().unwrap()).unwrap();
         let db = rocksdb::TransactionDB::open_default(tmpfile.path()).unwrap();
         let table = RocksdbBenchDatabase::new(&db);
-        benchmark(table)
+        populate(table)
     };
 
     let sled_results = {
         let tmpfile: TempDir = tempfile::tempdir_in(current_dir().unwrap()).unwrap();
         let db = sled::Config::new().path(tmpfile.path()).open().unwrap();
         let table = SledBenchDatabase::new(&db, tmpfile.path());
-        benchmark(table)
+        populate(table)
     };
 
     let sanakirja_results = {
@@ -267,7 +136,7 @@ fn main() {
         fs::remove_file(tmpfile.path()).unwrap();
         let db = sanakirja::Env::new(tmpfile.path(), 4096 * 1024 * 1024, 2).unwrap();
         let table = SanakirjaBenchDatabase::new(&db);
-        benchmark(table)
+        populate(table)
     };
 
     let mut rows = Vec::new();
