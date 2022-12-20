1819d33ed4331778e05d7d776824d78c92655dba
Christopher Berner <christopherberner@gmail.com>
2022-12-19 20:39:37 -0800

    Refinements to userspace_cache_benchmark

--- benches/userspace_cache_benchmark.rs
+++ benches/userspace_cache_benchmark.rs
@@ -13,14 +13,14 @@ mod unix {
     use std::ops::DerefMut;
     use std::os::unix::fs::FileExt;
     use std::path::Path;
-    use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
-    use std::sync::{Arc, Mutex, MutexGuard, RwLock};
+    use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicU64, AtomicUsize, Ordering};
+    use std::sync::{Arc, Mutex, RwLock};
     use std::time::{Duration, SystemTime};
-    use std::{io, thread};
+    use std::{io, mem, thread};
     use std::{os::unix::io::AsRawFd, ptr, slice};
 
     const ITERATIONS: usize = 3;
-    const VALUE_SIZE: usize = 2024;
+    const VALUE_SIZE: usize = 4096;
     const ELEMENTS_SPACE: usize = 1_000_000;
     const ELEMENTS: usize = ELEMENTS_SPACE / 10;
 
@@ -68,48 +68,42 @@ mod unix {
         page_numbers
     }
 
-    #[repr(C, align(4096))]
-    struct AlignedPage([u8; PagedCachedFile::page_size() as usize]);
-
-    impl AlignedPage {
-        fn new() -> Self {
-            Self([0; 4096])
-        }
-    }
-
     struct WritablePage<'a> {
+        buffer: &'a Mutex<BTreeMap<u64, Arc<Vec<u8>>>>,
         page: u64,
-        guard: MutexGuard<'a, BTreeMap<u64, AlignedPage>>,
+        data: Vec<u8>,
     }
 
     impl<'a> WritablePage<'a> {
         fn mut_data(&mut self) -> &mut [u8] {
-            &mut self
-                .guard
-                .entry(self.page)
-                .or_insert_with(AlignedPage::new)
-                .0
+            &mut self.data
         }
     }
 
-    struct ReadablePage {
-        data: Arc<AlignedPage>,
-    }
-
-    impl ReadablePage {
-        fn data(&self) -> &[u8] {
-            &self.data.0
+    impl<'a> Drop for WritablePage<'a> {
+        fn drop(&mut self) {
+            let data = mem::take(&mut self.data);
+            assert!(self
+                .buffer
+                .lock()
+                .unwrap()
+                .insert(self.page, Arc::new(data))
+                .is_none());
         }
     }
 
     struct PagedCachedFile {
         file: File,
+        page_size: u64,
         max_read_cache_bytes: usize,
         read_cache_bytes: AtomicUsize,
-        _max_write_buffer_bytes: usize,
-        _write_buffer_bytes: AtomicUsize,
-        read_cache: Vec<RwLock<BTreeMap<u64, Arc<AlignedPage>>>>,
-        write_buffer: Vec<Mutex<BTreeMap<u64, AlignedPage>>>,
+        max_write_buffer_bytes: usize,
+        write_buffer_bytes: AtomicUsize,
+        _reads_total: AtomicU64,
+        _reads_hits: AtomicU64,
+        fsync_failed: AtomicBool,
+        read_cache: Vec<RwLock<BTreeMap<u64, Arc<Vec<u8>>>>>,
+        write_buffer: Mutex<BTreeMap<u64, Arc<Vec<u8>>>>,
     }
 
     impl PagedCachedFile {
@@ -120,25 +114,25 @@ mod unix {
                 .create(true)
                 .open(path)
                 .unwrap();
+            file.set_len(4 * 1024 * 1024 * 1024).unwrap();
 
             let mut read_cache = Vec::with_capacity(Self::lock_stripes());
             for _ in 0..Self::lock_stripes() {
                 read_cache.push(RwLock::new(BTreeMap::new()));
             }
 
-            let mut write_buffer = Vec::with_capacity(Self::lock_stripes());
-            for _ in 0..Self::lock_stripes() {
-                write_buffer.push(Mutex::new(BTreeMap::new()));
-            }
-
             Self {
                 file,
+                page_size: 4096,
                 max_read_cache_bytes,
                 read_cache_bytes: AtomicUsize::new(0),
-                _max_write_buffer_bytes: max_write_buffer_bytes,
-                _write_buffer_bytes: AtomicUsize::new(0),
+                max_write_buffer_bytes,
+                write_buffer_bytes: AtomicUsize::new(0),
+                _reads_total: Default::default(),
+                _reads_hits: Default::default(),
+                fsync_failed: Default::default(),
                 read_cache,
-                write_buffer,
+                write_buffer: Default::default(),
             }
         }
 
@@ -146,7 +140,7 @@ mod unix {
             131
         }
 
-        const fn page_size() -> u64 {
+        const fn page_size() -> usize {
             4096
         }
 
@@ -154,103 +148,147 @@ mod unix {
             65536
         }
 
-        fn read_page_direct(&self, page: u64) -> io::Result<AlignedPage> {
-            let mut buffer = AlignedPage::new();
-            let offset = page * Self::page_size();
-            self.file.read_exact_at(&mut buffer.0, offset)?;
+        #[inline]
+        fn check_fsync_failure(&self) -> io::Result<()> {
+            if self.fsync_failed.load(Ordering::Acquire) {
+                Err(io::Error::from(io::ErrorKind::Other))
+            } else {
+                Ok(())
+            }
+        }
+
+        #[inline]
+        fn set_fsync_failed(&self, failed: bool) {
+            self.fsync_failed.store(failed, Ordering::Release);
+        }
+
+        fn read_page_direct(&self, page: u64) -> io::Result<Vec<u8>> {
+            let mut buffer = vec![0; Self::page_size()];
+            let offset = page * self.page_size;
+            self.file.read_exact_at(&mut buffer, offset)?;
             Ok(buffer)
         }
 
-        fn read_page(&self, page: u64) -> io::Result<ReadablePage> {
+        fn read_page(&self, page: u64) -> io::Result<Arc<Vec<u8>>> {
+            self.check_fsync_failure()?;
+            // self.reads_total.fetch_add(1, Ordering::Release);
+
             let cache_slot = page as usize % self.read_cache.len();
             {
                 let read_lock = self.read_cache[cache_slot].read().unwrap();
                 if let Some(cached) = read_lock.get(&page) {
-                    return Ok(ReadablePage {
-                        data: cached.clone(),
-                    });
+                    // self.reads_hits.fetch_add(1, Ordering::Release);
+                    return Ok(cached.clone());
                 }
             }
 
             let buffer = Arc::new(self.read_page_direct(page)?);
             let cache_size = self
                 .read_cache_bytes
-                .fetch_add(buffer.0.len(), Ordering::AcqRel);
+                .fetch_add(buffer.len(), Ordering::AcqRel);
             let mut write_lock = self.read_cache[cache_slot].write().unwrap();
             write_lock.insert(page, buffer.clone());
             let mut removed = 0;
-            if cache_size + buffer.0.len() > self.max_read_cache_bytes {
-                while removed < buffer.0.len() {
+            if cache_size + buffer.len() > self.max_read_cache_bytes {
+                while removed < buffer.len() {
                     let k = *write_lock.iter().next().unwrap().0;
                     let v = write_lock.remove(&k).unwrap();
-                    removed += v.0.len();
+                    removed += v.len();
                 }
             }
             if removed > 0 {
                 self.read_cache_bytes.fetch_sub(removed, Ordering::AcqRel);
             }
 
-            Ok(ReadablePage { data: buffer })
+            Ok(buffer)
         }
 
         fn write_page(&self, page: u64) -> WritablePage {
+            let mut lock = self.write_buffer.lock().unwrap();
+
+            let existing = None;
+
+            let data = if let Some(removed) = lock.remove(&page) {
+                Arc::try_unwrap(removed).unwrap()
+            } else {
+                let previous = self
+                    .write_buffer_bytes
+                    .fetch_add(Self::page_size(), Ordering::AcqRel);
+                if previous + Self::page_size() > self.max_write_buffer_bytes {
+                    let mut removed_bytes = 0usize;
+                    while removed_bytes < Self::page_size() {
+                        if let Some((offset, buffer)) = lock.pop_first() {
+                            self.write_buffer_bytes
+                                .fetch_sub(buffer.len(), Ordering::Release);
+                            removed_bytes += buffer.len();
+                            self.file.write_all_at(&buffer, offset).unwrap();
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                if let Some(data) = existing {
+                    data
+                } else {
+                    self.read_page_direct(page).unwrap()
+                }
+            };
             WritablePage {
+                buffer: &self.write_buffer,
                 page,
-                guard: self.write_buffer[page as usize % Self::lock_stripes()]
-                    .lock()
-                    .unwrap(),
+                data,
             }
         }
 
         fn flush2(&mut self) -> io::Result<()> {
-            for stripe in 0..Self::lock_stripes() {
-                let x: BTreeMap<u64, AlignedPage> = BTreeMap::new();
-                let write_buffer =
-                    std::mem::replace(self.write_buffer[stripe].lock().unwrap().deref_mut(), x);
-                let mut batch: Vec<IoSlice> = vec![];
-                let mut batch_start_offset = None;
-                let mut batch_last_page = None;
-                let mut iter = write_buffer.iter();
-                loop {
-                    let entry = iter.next();
-                    if entry.is_none() {
-                        // submit batch
-                        self.file
-                            .seek(SeekFrom::Start(batch_start_offset.unwrap()))?;
-                        let written = self.file.write_vectored(&batch)?;
-                        assert_eq!(written, batch.len() * Self::page_size() as usize);
-                        break;
-                    }
-                    let (page, buffer) = entry.unwrap();
-                    let offset = *page * Self::page_size();
-                    if batch_start_offset.is_none() {
-                        batch_start_offset = Some(offset);
-                        batch_last_page = Some(page);
-                        assert!(batch.is_empty());
-                        batch.push(IoSlice::new(&buffer.0));
-                    } else if *batch_last_page.unwrap() == page - 1
-                        && batch.len() * (Self::page_size() as usize)
-                            < Self::target_write_size() as usize
-                    {
-                        batch_last_page = Some(page);
-                        batch.push(IoSlice::new(&buffer.0));
-                    } else {
-                        // submit batch
-                        self.file
-                            .seek(SeekFrom::Start(batch_start_offset.unwrap()))?;
-                        let written = self.file.write_vectored(&batch)?;
-                        assert_eq!(written, batch.len() * Self::page_size() as usize);
+            let x: BTreeMap<u64, Arc<Vec<u8>>> = BTreeMap::new();
+            let write_buffer = std::mem::replace(self.write_buffer.lock().unwrap().deref_mut(), x);
+            let mut batch: Vec<IoSlice> = vec![];
+            let mut batch_start_offset = None;
+            let mut batch_last_page = None;
+            let mut iter = write_buffer.iter();
+            loop {
+                let entry = iter.next();
+                if entry.is_none() {
+                    // submit batch
+                    self.file
+                        .seek(SeekFrom::Start(batch_start_offset.unwrap()))?;
+                    let written = self.file.write_vectored(&batch)?;
+                    assert_eq!(written, batch.len() * Self::page_size());
+                    break;
+                }
+                let (page, buffer) = entry.unwrap();
+                let offset = *page * self.page_size;
+                if batch_start_offset.is_none() {
+                    batch_start_offset = Some(offset);
+                    batch_last_page = Some(page);
+                    assert!(batch.is_empty());
+                    batch.push(IoSlice::new(buffer));
+                } else if *batch_last_page.unwrap() == page - 1
+                    && batch.len() * Self::page_size() < Self::target_write_size() as usize
+                {
+                    batch_last_page = Some(page);
+                    batch.push(IoSlice::new(buffer));
+                } else {
+                    // submit batch
+                    self.file
+                        .seek(SeekFrom::Start(batch_start_offset.unwrap()))?;
+                    let written = self.file.write_vectored(&batch)?;
+                    assert_eq!(written, batch.len() * Self::page_size());
 
-                        // Enqueue this entry
-                        batch.clear();
-                        batch_start_offset = Some(offset);
-                        batch_last_page = Some(page);
-                        batch.push(IoSlice::new(&buffer.0));
-                    }
+                    // Enqueue this entry
+                    batch.clear();
+                    batch_start_offset = Some(offset);
+                    batch_last_page = Some(page);
+                    batch.push(IoSlice::new(buffer));
                 }
             }
 
-            self.file.sync_all()
+            let res = self.file.sync_all();
+            if res.is_err() {
+                self.set_fsync_failed(true);
+            }
+            res
         }
     }
 
@@ -457,14 +495,12 @@ mod unix {
                 let pairs_len = pairs2.len();
                 for &i in &chunk {
                     let value = &pairs2[i % pairs_len];
-                    let page_num = i / 2;
-                    let offset = VALUE_SIZE * (i % 2);
                     if direct {
-                        let page = file2.read_page_direct(page_num as u64).unwrap();
-                        checksum += page.0[offset] as u64;
+                        let page = file2.read_page_direct(i as u64).unwrap();
+                        checksum += page[0] as u64;
                     } else {
-                        let page = file2.read_page(page_num as u64).unwrap();
-                        checksum += page.data()[offset] as u64;
+                        let page = file2.read_page(i as u64).unwrap();
+                        checksum += page[0] as u64;
                     }
                     expected_checksum += value[0] as u64;
                 }
@@ -509,7 +545,7 @@ mod unix {
         ));
 
         // We assume two values fit into a single page
-        assert!(2 * VALUE_SIZE <= PagedCachedFile::page_size() as usize);
+        assert_eq!(VALUE_SIZE, PagedCachedFile::page_size());
 
         let pairs = Arc::new(gen_data(1000, VALUE_SIZE));
         let pairs_len = pairs.len();
@@ -529,11 +565,8 @@ mod unix {
             let t = thread::spawn(move || {
                 for i in chunk {
                     let value = &pairs2[i % pairs_len];
-                    let page_num = i / 2;
-                    let offset = VALUE_SIZE * (i % 2);
-                    let mut page = file2.write_page(page_num as u64);
-                    let data = page.mut_data();
-                    data[offset..(offset + value.len())].copy_from_slice(value);
+                    let mut page = file2.write_page(i as u64);
+                    page.mut_data().copy_from_slice(value);
                 }
             });
             thread_joins.push(t);
