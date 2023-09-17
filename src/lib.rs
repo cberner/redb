5be2bfdf740347d8f1e0c38acbc901237cca12e4
Christopher Berner <me@cberner.com>
2023-09-17 08:46:12 -0700

    Prevent tests from creating my_db.redb in working directory

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,10 @@
 //!
 //! const TABLE: TableDefinition<&str, u64> = TableDefinition::new("my_data");
 //!
+//! #[cfg(not(target_os = "wasi"))]
 //! fn main() -> Result<(), Error> {
-//!     let db = Database::create("my_db.redb")?;
+//!     let file = tempfile::NamedTempFile::new().unwrap();
+//!     let db = Database::create(file.path())?;
 //!     let write_txn = db.begin_write()?;
 //!     {
 //!         let mut table = write_txn.open_table(TABLE)?;
