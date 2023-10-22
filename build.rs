bce2b453d2a90fe3a25d4b6fd6a8b7d5292cd286
Douglas Dwyer <dougmdwyer@hotmail.com>
2023-10-21 21:00:01 -0400

    Fix fuzzing link issue on Mac

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
+    if std::env::var("CARGO_CFG_FUZZING").is_ok() && std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("macos") {
+        println!("cargo:rustc-cdylib-link-arg=-undefined");
+        println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+    }
+
     #[cfg(feature = "python")]
     pyo3_build_config::add_extension_module_link_args();
 }
