0fe972970ee6abc45a860f75998c53f31a78d3f3
Douglas Dwyer <dougmdwyer@hotmail.com>
2023-10-21 21:23:29 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    if std::env::var("CARGO_CFG_FUZZING").is_ok() && std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("macos") {
+    if std::env::var("CARGO_CFG_FUZZING").is_ok()
+        && std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("macos")
+    {
         println!("cargo:rustc-cdylib-link-arg=-undefined");
         println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
     }
