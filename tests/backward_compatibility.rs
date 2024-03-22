14ba17964ece214083ebc05f6adde9f812bef981
Christopher Berner <me@cberner.com>
2024-03-22 10:43:18 -0700

    Update and re-enable backwards compatibility tests

--- tests/backward_compatibility.rs
+++ tests/backward_compatibility.rs
@@ -3,188 +3,250 @@ use redb1::ReadableTable as ReadableTable1;
 
 const ELEMENTS: usize = 3;
 
-trait TestData: redb::Value + redb1::RedbValue {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS];
+trait TestData: redb::Value + redb2::Value {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a;
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS];
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a;
 }
 
 impl TestData for u8 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u16 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u32 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u64 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u128 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for i8 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i16 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i32 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i64 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i128 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for f32 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [f32::NAN, f32::INFINITY, f32::MIN_POSITIVE]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [f32::NAN, f32::INFINITY, f32::MIN_POSITIVE]
     }
 }
 
 impl TestData for f64 {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [f64::MIN, f64::NEG_INFINITY, f64::MAX]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [f64::MIN, f64::NEG_INFINITY, f64::MAX]
     }
 }
 
 impl TestData for () {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [(), (), ()]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [(), (), ()]
     }
 }
 
 impl TestData for &'static str {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         ["hello", "world1", "hi"]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         ["hello", "world1", "hi"]
     }
 }
 
 impl TestData for &'static [u8] {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [b"test", b"bytes", b"now"]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [b"test", b"bytes", b"now"]
     }
 }
 
 impl TestData for &'static [u8; 5] {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [b"test1", b"bytes", b"now12"]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [b"test1", b"bytes", b"now12"]
     }
 }
 
+impl TestData for [&str; 3] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a,
+    {
+        [
+            ["test1", "hi", "world"],
+            ["test2", "hi", "world"],
+            ["test3", "hi", "world"],
+        ]
+    }
+
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a,
+    {
+        [
+            ["test1", "hi", "world"],
+            ["test2", "hi", "world"],
+            ["test3", "hi", "world"],
+        ]
+    }
+}
+
+impl TestData for [u128; 3] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+        [[1, 2, 3], [3, 2, 1], [300, 200, 100]]
+    }
+
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+        [[1, 2, 3], [3, 2, 1], [300, 200, 100]]
+    }
+}
+
+impl TestData for Vec<&str> {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a,
+    {
+        [
+            vec!["test1", "hi", "world"],
+            vec!["test2", "hi", "world"],
+            vec!["test3", "hi", "world"],
+        ]
+    }
+
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    where
+        Self: 'a,
+    {
+        [
+            vec!["test1", "hi", "world"],
+            vec!["test2", "hi", "world"],
+            vec!["test3", "hi", "world"],
+        ]
+    }
+}
+
 impl TestData for Option<u64> {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [None, Some(0), Some(7)]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [None, Some(0), Some(7)]
     }
 }
 
 impl TestData for (u64, &'static str) {
-    fn gen1() -> [<Self as redb1::RedbValue>::SelfType<'static>; ELEMENTS] {
+    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [(0, "hi"), (1, "bye"), (2, "byte")]
     }
 
-    fn gen() -> [<Self as redb::Value>::SelfType<'static>; ELEMENTS] {
+    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [(0, "hi"), (1, "bye"), (2, "byte")]
     }
 }
@@ -197,15 +259,15 @@ fn create_tempfile() -> tempfile::NamedTempFile {
     }
 }
 
-fn test_helper<K: TestData + redb::Key + redb1::RedbKey + 'static, V: TestData + 'static>() {
+fn test_helper<K: TestData + redb::Key + redb2::Key + 'static, V: TestData + 'static>() {
     let tmpfile = create_tempfile();
-    let db = redb1::Database::create(tmpfile.path()).unwrap();
-    let table_def: redb1::TableDefinition<K, V> = redb1::TableDefinition::new("table");
+    let db = redb2::Database::create(tmpfile.path()).unwrap();
+    let table_def: redb2::TableDefinition<K, V> = redb2::TableDefinition::new("table");
     let write_txn = db.begin_write().unwrap();
     {
         let mut table = write_txn.open_table(table_def).unwrap();
         for i in 0..ELEMENTS {
-            table.insert(&K::gen1()[i], &V::gen1()[i]).unwrap();
+            table.insert(&K::gen2()[i], &V::gen2()[i]).unwrap();
         }
     }
     write_txn.commit().unwrap();
@@ -226,9 +288,7 @@ fn test_helper<K: TestData + redb::Key + redb1::RedbKey + 'static, V: TestData +
     }
 }
 
-// TODO: re-enable
 #[test]
-#[ignore]
 fn primitive_types() {
     test_helper::<u8, u8>();
     test_helper::<u16, u16>();
@@ -246,19 +306,17 @@ fn primitive_types() {
     test_helper::<u8, ()>();
 }
 
-// TODO: re-enable
 #[test]
-#[ignore]
 fn container_types() {
     test_helper::<&[u8], &[u8]>();
     test_helper::<&[u8; 5], &[u8; 5]>();
     test_helper::<u64, Option<u64>>();
     test_helper::<(u64, &str), &str>();
+    test_helper::<[&str; 3], [u128; 3]>();
+    test_helper::<u64, Vec<&str>>();
 }
 
-// TODO: re-enable
 #[test]
-#[ignore]
 fn mixed_width() {
     test_helper::<u8, &[u8]>();
     test_helper::<&[u8; 5], &str>();
