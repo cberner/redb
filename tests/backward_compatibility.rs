97fe4fab0c00fb8d94fe2e9e5c005df61521cd58
Christopher Berner <me@cberner.com>
2025-02-20 17:19:58 -0800

    Rename methods to prep for edition 2024

--- tests/backward_compatibility.rs
+++ tests/backward_compatibility.rs
@@ -4,177 +4,177 @@ use redb1::ReadableTable as ReadableTable1;
 const ELEMENTS: usize = 3;
 
 trait TestData: redb::Value + redb2::Value {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a;
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a;
 }
 
 impl TestData for u8 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u16 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u32 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u64 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for u128 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [0, 1, 2]
     }
 }
 
 impl TestData for i8 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i16 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i32 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i64 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for i128 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [-1, 1, 2]
     }
 }
 
 impl TestData for f32 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [f32::NAN, f32::INFINITY, f32::MIN_POSITIVE]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [f32::NAN, f32::INFINITY, f32::MIN_POSITIVE]
     }
 }
 
 impl TestData for f64 {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [f64::MIN, f64::NEG_INFINITY, f64::MAX]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [f64::MIN, f64::NEG_INFINITY, f64::MAX]
     }
 }
 
 impl TestData for () {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [(), (), ()]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [(), (), ()]
     }
 }
 
 impl TestData for &'static str {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         ["hello", "world1", "hi"]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         ["hello", "world1", "hi"]
     }
 }
 
 impl TestData for &'static [u8] {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [b"test", b"bytes", b"now"]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [b"test", b"bytes", b"now"]
     }
 }
 
 impl TestData for &'static [u8; 5] {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [b"test1", b"bytes", b"now12"]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [b"test1", b"bytes", b"now12"]
     }
 }
 
 impl TestData for [&str; 3] {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a,
     {
@@ -185,7 +185,7 @@ impl TestData for [&str; 3] {
         ]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a,
     {
@@ -198,17 +198,17 @@ impl TestData for [&str; 3] {
 }
 
 impl TestData for [u128; 3] {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [[1, 2, 3], [3, 2, 1], [300, 200, 100]]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [[1, 2, 3], [3, 2, 1], [300, 200, 100]]
     }
 }
 
 impl TestData for Vec<&str> {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a,
     {
@@ -219,7 +219,7 @@ impl TestData for Vec<&str> {
         ]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS]
     where
         Self: 'a,
     {
@@ -232,21 +232,21 @@ impl TestData for Vec<&str> {
 }
 
 impl TestData for Option<u64> {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [None, Some(0), Some(7)]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [None, Some(0), Some(7)]
     }
 }
 
 impl TestData for (u64, &'static str) {
-    fn gen2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data_v2<'a>() -> [<Self as redb2::Value>::SelfType<'a>; ELEMENTS] {
         [(0, "hi"), (1, "bye"), (2, "byte")]
     }
 
-    fn gen<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
+    fn make_data<'a>() -> [<Self as redb::Value>::SelfType<'a>; ELEMENTS] {
         [(0, "hi"), (1, "bye"), (2, "byte")]
     }
 }
@@ -267,7 +267,9 @@ fn test_helper<K: TestData + redb::Key + redb2::Key + 'static, V: TestData + 'st
     {
         let mut table = write_txn.open_table(table_def).unwrap();
         for i in 0..ELEMENTS {
-            table.insert(&K::gen2()[i], &V::gen2()[i]).unwrap();
+            table
+                .insert(&K::make_data_v2()[i], &V::make_data_v2()[i])
+                .unwrap();
         }
     }
     write_txn.commit().unwrap();
@@ -279,10 +281,10 @@ fn test_helper<K: TestData + redb::Key + redb2::Key + 'static, V: TestData + 'st
     let table = read_txn.open_table(table_def).unwrap();
     assert_eq!(table.len().unwrap(), ELEMENTS as u64);
     for i in 0..ELEMENTS {
-        let result = table.get(&K::gen()[i]).unwrap().unwrap();
+        let result = table.get(&K::make_data()[i]).unwrap().unwrap();
         let value = result.value();
         let bytes = <V as redb::Value>::as_bytes(&value);
-        let expected = &V::gen()[i];
+        let expected = &V::make_data()[i];
         let expected_bytes = <V as redb::Value>::as_bytes(expected);
         assert_eq!(bytes.as_ref(), expected_bytes.as_ref());
     }
